37f27802c418ddb59d51829b85a1fdff13c1604a
Yol-B <yoltorres24@gmail.com>
2020-02-10 20:56:46 +0800

    cannot get user

---
+++ resources/assets/js/components/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="box-form">
+    <div class="left">
+      <div class="overlay">
+        <h1>Hello World.</h1>
+      </div>
+    </div>
+
+    <div class="right">
+      <h5>Login</h5>
+      <br />
+      <div class="inputs">
+        <input type="text" v-model="name" placeholder="name" />
+	  <br>
+        <input type="email" v-model="email" placeholder="email" />
+        <br />
+        <input type="password" v-model="password" placeholder="password" />
+      </div>
+      <br />
+      <br />
+
+      <div class="remember-me--forget-password">
+        <label>
+          <input type="checkbox" name="item" checked />
+          <span class="text-checkbox">Remember me</span>
+        </label>
+        <p>forget password?</p>
+      </div>
+      <br />
+      <button @click="login"> Login</button>
+      <button @click="register"> Register</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			email:"",
+			name:"",
+			password:""
+		}
+	},
+	methods: {
+		register(){
+			axios.post("/api/register" , {
+				name:this.name,
+				email:this.email,
+				password:this.password
+			})
+		},
+		login(){
+			axios.post("/api/login" , {
+				email:this.email,
+				password:this.password
+			})
+		}
+	},
+};
+</script>
